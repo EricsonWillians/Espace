26b95b44d9bfc5100c5108343a25832332daa3f6
Ericson Willians Rocha Pires <ericsonwrp@gmail.com>
2014-08-19 19:03:05 -0300

    Added licence.

--- run.py
+++ run.py
@@ -1,3 +1,32 @@
+"""
+====================================================================
+
+ESPACE
+Copyright (C) <2014>  <Ericson Willians.>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+====================================================================
+Game written by Ericson Willians, a brazilian composer and programmer.
+
+CONTACT: ericsonwrp@gmail.com
+AS A COMPOSER: http://www.youtube.com/user/poisonewein
+TWITTER: https://twitter.com/poisonewein
+
+====================================================================
+"""
+
 import os
 import pygame
 import ergame as er
